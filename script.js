ae21b3da26679711764b6365971e5ef57c84397c
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-04-14 12:23:28 +0100

    Initialized theme button creation

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function changeTheme() {
+    let body = document.querySelector("body");
+}
+
+let themeButton = document.querySelector("#theme-button");
+themeButton.addEventListener('click', changeTheme);
