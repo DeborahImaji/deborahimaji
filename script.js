5364d02771789cdc93870df89d75338ae19b20f2
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-05-15 14:41:41 +0100

    Broke the theme button while changing icons

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 function changeTheme() {
     let body = document.querySelector("body");
+    let themeButtonIcon = document.querySelector("#theme=button-icon");
 
     if (body.classList.contains("dark")) {
         body.classList.remove("dark");
+        themeButtonIcon.innerHTML = `<img src="assets/icons/sun.png" alt="Sun Icon" class="theme-button-icon" id="theme-button-icon">`;
     } else {
         body.classList.add("dark");
+        themeButtonIcon.innerHTML = `<img src="assets/icons/moon.png" alt="Moon Icon" class="theme-button-icon" id="theme-button-icon">`;
     }
 }
 
