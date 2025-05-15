58fa8c3a90ca4e1a2f12f20cefa71a456e50a67b
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-05-15 16:17:08 +0100

    Fixed the broken theme button

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
 function changeTheme() {
     let body = document.querySelector("body");
-    let themeButtonIcon = document.querySelector("#theme=button-icon");
 
     if (body.classList.contains("dark")) {
         body.classList.remove("dark");
-        themeButtonIcon.innerHTML = `<img src="assets/icons/sun.png" alt="Sun Icon" class="theme-button-icon" id="theme-button-icon">`;
+        themeButton.innerHTML = `<img src="assets/icons/sun.png" alt="Sun Icon" class="theme-button-icon" id="theme-button-icon">`;
     } else {
         body.classList.add("dark");
-        themeButtonIcon.innerHTML = `<img src="assets/icons/moon.png" alt="Moon Icon" class="theme-button-icon" id="theme-button-icon">`;
+        themeButton.innerHTML = `<img src="assets/icons/moon.png" alt="Moon Icon" class="theme-button-icon" id="theme-button-icon">`;
     }
 }
 
