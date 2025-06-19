b058b8373fe7bc0dbaf455665dbe41124428c2b5
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-06-19 17:19:04 +0100

    Added the mail icon for the navbar social links

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 function changeTheme(event) {
     let bodyElement = document.querySelector("body");
+    let mailIcon = document.querySelector("#mail-icon");
 
     if (bodyElement.classList.contains("dark")) {
         bodyElement.classList.remove("dark");
+        mailIcon.src = "assets/icons/socials/email.png";
     } else {
         bodyElement.classList.add("dark");
+        mailIcon.src = "assets/icons/socials/email-white.png";
     }
 }
 
