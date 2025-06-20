18e10fae8144c7a38097891f502e872e3a248818
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-06-20 09:24:36 +0100

    Finished building the navbar social icons

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
 function changeTheme(event) {
     let bodyElement = document.querySelector("body");
+
     let mailIcon = document.querySelector("#mail-icon");
+    let githubIcon = document.querySelector("#github-icon");
+    let linkedinIcon = document.querySelector("#linkedin-icon");
 
     if (bodyElement.classList.contains("dark")) {
+
         bodyElement.classList.remove("dark");
+        themeButton.classList.remove("dark");
+
         mailIcon.src = "assets/icons/socials/email.png";
+        githubIcon.src = "assets/icons/socials/github.png";
+        linkedinIcon.src = "assets/icons/socials/linkedin.png";
+
     } else {
+
         bodyElement.classList.add("dark");
+        themeButton.classList.add("dark");
+
         mailIcon.src = "assets/icons/socials/email-white.png";
+        githubIcon.src = "assets/icons/socials/github-white.png";
+        linkedinIcon.src = "assets/icons/socials/linkedin-white.png";
+
     }
 }
 
