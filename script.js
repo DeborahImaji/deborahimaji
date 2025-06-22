0d09c0fb4ae6416684a04d42d4a57fb81fcb1793
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-06-22 19:53:06 +0100

    Finished coding the navbar
    However, fixed positioning will be added later.

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 
 function changeTheme(event) {
+    event.preventDefault();
     let bodyElement = document.querySelector("body");
 
     let mailIcon = document.querySelector("#mail-icon");
     let githubIcon = document.querySelector("#github-icon");
     let linkedinIcon = document.querySelector("#linkedin-icon");
 
+    let themeIcon = document.querySelector("#theme-icon");
+
     if (bodyElement.classList.contains("dark")) {
 
         bodyElement.classList.remove("dark");
-        themeButton.classList.remove("dark");
 
         mailIcon.src = "assets/icons/socials/email.png";
         githubIcon.src = "assets/icons/socials/github.png";
         linkedinIcon.src = "assets/icons/socials/linkedin.png";
 
+        themeIcon.src = "assets/icons/theme/moon.png";
+
     } else {
 
         bodyElement.classList.add("dark");
-        themeButton.classList.add("dark");
 
         mailIcon.src = "assets/icons/socials/email-white.png";
         githubIcon.src = "assets/icons/socials/github-white.png";
         linkedinIcon.src = "assets/icons/socials/linkedin-white.png";
+
+        themeIcon.src = "assets/icons/theme/sun.png";
 
     }
 }
