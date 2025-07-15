d00ec5c673270bf61cfcc18334aa8ecea4ecb850
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-07-15 15:54:30 +0100

    Began writing HTML for intro

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeTheme(event) {
+    event.preventDefault();
+
+    let bodyElement = document.querySelector("body");
+
+    if (bodyElement.classList.contains("light")) {
+        bodyElement.classList.remove("light");
+    } else {
+        bodyElement.classList.add("light");
+    }
+}
+
+let themeButton = document.querySelector("#theme-button");
+themeButton.addEventListener('click', changeTheme);
