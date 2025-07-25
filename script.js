d233db8f6864f0e3634fc65cb2e88c8619f896bb
Deborah Imaji <101178292+DeborahImaji@users.noreply.github.com>
2025-07-25 15:29:27 +0100

    Writing navigation bar CSS

--- script.js
+++ script.js
@@ -10,5 +10,20 @@ function changeTheme (event) {
     }
 }
 
+function setCurrentHome (event) {
+    event.preventDefault();
+
+    navHome = document.querySelector("#nav-link");
+
+    if (navHome.classList.contains("current")) {
+        navHome.classList.remove("current");
+    } else {
+        navHome.classList.add("current");
+    }
+}
+
+let navHome = document.querySelector("#nav-link");
+navHome.addEventListener('click', setCurrentHome);
+
 let themeButton = document.querySelector("#theme-button");
 themeButton.addEventListener('click', changeTheme);
